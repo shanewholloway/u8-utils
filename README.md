dd4d7d2759439a17ad9c6a2794d55d34b07f442e
Shane Holloway <shane.holloway@ieee.org>
2018-10-04 11:59:37 -0600

    Combining Uint8Array utilities from various projects into a module-friendly composite project

---
+++ README.md
@@ -0,0 +1,7 @@
+# u8-utils
+
+Uint8Array + UTF8 string utility library for NodeJS and the Web
+
+## License
+
+[BSD 2-clause](LICENSE)
---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import pkg from './package.json'
+import rpi_jsy from 'rollup-plugin-jsy-lite'
+const pkg_name = pkg.name.replace('-', '_')
+
+const configs = []
+export default configs
+
+const sourcemap = true
+const external = []
+
+const plugins = []
+const plugins_nodejs = [ rpi_jsy({defines: {PLAT_NODEJS: true}}) ].concat(plugins)
+const plugins_web = [ rpi_jsy({defines: {PLAT_WEB: true}}) ].concat(plugins)
+const plugins_min = plugins_web.slice()
+
+import { terser as rpi_terser } from 'rollup-plugin-terser'
+plugins_min.push(rpi_terser({}))
+
+
+add_jsy('index', true)
+add_jsy('hex')
+add_jsy('base64')
+add_jsy('utf8')
+add_jsy('random')
+add_jsy('buffer')
+
+
+function add_jsy(src_name, inc_min) {
+  const module_name = inc_min ? pkg_name : `${pkg_name}-${src_name}`
+
+  if (plugins_nodejs)
+    configs.push({
+      input: `code/${src_name}.jsy`,
+      plugins: plugins_nodejs, external,
+      output: [
+        { file: `cjs/${src_name}.js`, format: 'cjs', exports:'named', sourcemap },
+        { file: `esm/${src_name}.js`, format: 'es', sourcemap } ]})
+
+  if (plugins_web)
+    configs.push({
+      input: `code/${src_name}.jsy`,
+      plugins: plugins_web, external,
+      output: [
+        { file: `umd/${src_name}${inc_min ? '.dbg' : ''}.js`, format: 'umd', name:module_name, exports:'named', sourcemap },
+        { file: `esm/web/${src_name}.js`, format: 'es', sourcemap } ]})
+
+  if (inc_min && plugins_min)
+    configs.push({
+      input: `code/${src_name}.jsy`,
+      plugins: plugins_min, external,
+      output: { file: `umd/${src_name}.min.js`, format: 'umd', name:module_name, exports:'named', sourcemap }})
+}
