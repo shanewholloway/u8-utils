bdcfada7b995a8e415730d3c0a727db84befb0d1
Shane Holloway <shane.holloway@ieee.org>
2020-02-24 10:27:02 -0700

    +60% faster hex_to_u8 impl

--- test/rollup.config.js
+++ test/rollup.config.js
@@ -1,6 +1,9 @@
 import rpi_resolve from '@rollup/plugin-node-resolve'
 import rpi_jsy from 'rollup-plugin-jsy-lite'
 
+const configs = []
+export default configs
+
 const sourcemap = 'inline'
 
 const plugins = [ rpi_resolve() ]
@@ -11,15 +14,20 @@ const plugins_web = [
   rpi_jsy({defines: {PLAT_WEB: true}},
   ... plugins) ]
 
-
-export default [
+configs.push(
   { input: `./unittest.jsy`, plugins: plugins_nodejs,
     output: { file: './__unittest.cjs.js', format: 'cjs', sourcemap } },
   { input: `./unittest.jsy`, context: 'window', plugins: plugins_web,
     output: { file: './__unittest.iife.js', format: 'iife', name: `test_u8_utils`, sourcemap } },
+)
 
-  { input: `./bench/hex.jsy`, context: 'window', plugins: plugins_web,
-    output: { file: './bench/iife/hex.js', format: 'iife', name: `bench_hex`, sourcemap } },
-  { input: `./bench/hex.jsy`, plugins: plugins_nodejs,
-    output: { file: './bench/cjs/hex.cjs', format: 'cjs', sourcemap } },
-]
+add_bench('hex_encode')
+add_bench('hex_decode')
+
+function add_bench(src_name, opt={}) {
+  configs.push(
+    { input: `./bench/${src_name}.jsy`, context: 'window', plugins: plugins_web,
+      output: { file: `./bench/iife/${src_name}.js`, format: 'iife', name: `bench_${src_name}`, sourcemap } },
+    { input: `./bench/${src_name}.jsy`, plugins: plugins_nodejs,
+      output: { file: `./bench/cjs/${src_name}.cjs`, format: 'cjs', sourcemap } }, )
+}
